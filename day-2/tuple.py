e8ae5abebc1a703c45d3d13eaf13a0864866adb9
pateldipen26 <pateldipen2607@gmail.com>
2025-11-26 17:39:44 +0530

    updated code

---
+++ day-2/tuple.py
@@ -0,0 +1,29 @@
+d = (26,22,45,49,45,67,58,98,45,"dipen","zalak","patel")
+print(d)
+
+no = d.count(45) # counting for repeatation
+print(no)
+
+mo = d.index(26) # indexsing the number in tuple
+print(mo)
+
+repeated = d * 4 # repeate the valu you ask time
+print(repeated)
+
+print(49 in d)  # checking the valu in tuple
+print(345 in d)
+print(len(d))
+
+sliced = d[0:5] # used for slice
+print (sliced)
+
+y = (89,45,76)
+a, b, c = y
+print(a, b, c)
+print(a)
+
+f = (882,235,580,239,481,273,825,895,567,76,323,874,7,904,82,725,311)
+
+p = f.count(273)
+
+print(p)
